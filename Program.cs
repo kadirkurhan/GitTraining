7c6e463e92273f170fc4a2dc8575b61ac468e148
Kadir KURHAN <kadir.kurhan@veripark.com>
2024-05-24 13:43:55 +0300

    [BUG]: FEATURE/AddRateLimiting#4256

--- Program.cs
+++ Program.cs
@@ -16,24 +16,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return 1;
 })
-.WithName("GetWeatherForecast")
+.WithName("Hello")
 .WithOpenApi();
 
 app.Run();
